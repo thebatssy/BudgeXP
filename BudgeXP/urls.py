e9ccfe2ed26b4732eadf865175ed6ceddff8fbf5
Gaurav Singh Bhadouriya <gauravbhadouriya13@gmail.com>
2026-09-24 17:23:40 +0530

    redirecting visitors from root to interactive swagger UI docs

--- BudgeXP/urls.py
+++ BudgeXP/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('expenses.urls')),
+    
+    # Redirect root URL (/) to API docs (/api/docs/)
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
     
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
